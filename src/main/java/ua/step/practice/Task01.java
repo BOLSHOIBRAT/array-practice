b57e0d4a9da1bf87ad07a3ec965873592d2388bd
BOLSHOIBRAT <71118008+bolshoibrat@users.noreply.github.com>
2020-09-18 11:41:55 +0300

    Решено первое задание на числа фиббоначи

--- src/main/java/ua/step/practice/Task01.java
+++ src/main/java/ua/step/practice/Task01.java
@@ -10,7 +10,15 @@ package ua.step.practice;
 public class Task01 {
     public static void main(String[] args) {
         // TODO: Пишите код здесь
-
-
+        int [] fib = new int[15];
+        fib [0] = 1;
+        fib [1] = 1;
+        for (int i = 2; i < fib.length; i++) {
+            fib[i] = fib[i - 1] + fib [i - 2];
+        }
+        for (int i = 0; i < fib.length; i += 2) {
+        //if (i % 2 == 0) {
+            System.out.print (fib[i] + " ");
+        }
+        }
     }
-}
